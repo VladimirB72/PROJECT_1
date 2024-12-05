df7a1e5be77b4fbc9ac70dbf11fd8ceec7b553c1
VladimirB72 <borek.vladimir@iex.cz>
2024-12-05 17:54:01 +0100

    zamestnanci

---
+++ 2_lekce_zamestnanci_vkadani_jmen.py
@@ -0,0 +1,23 @@
+# vytvořím list zamestnaci
+zamestnanci = [
+    "František", "Anna",
+    "Jakub", "Klára"
+] 
+
+# vpis zamestnanci na začátku
+print("Zaměstnanci na začátku:", zamestnanci)
+
+# vytvoření kopie listu zamestnanci
+zamestnanci_a = zamestnanci.copy()
+zamestnanci_a.append("Bruno")
+zamestnanci_a.append("Anežka")
+
+# výpis zaměstnanci + nová jména
+print("Nová jména přidána :" , zamestnanci_a)
+
+# vytvoření kopie zamestnanci , proměnná zamestnanci_b
+zamestnanci_b = zamestnanci.copy()
+zamestnanci_b.insert(1, "Bruno")
+
+vypiš zamestnanci_b ,aktualni list
+print("Nová jména vložena :" , zamestnanci_b)
